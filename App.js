f46f87fc2f66c7cffd87e4e186f18abd3a2ac6df
varunvasudevan94 <varunvasudevan11@gmail.com>
2020-10-25 11:14:39 +0530

    Basic functionalities to listen to db

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, Text, View, Button } from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
+import RNSoundLevel from 'react-native-sound-level';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +10,54 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  number: {
+    fontFamily: 'Lato',
+    fontSize: 200,
+    color: 'red',
+  },
+  button: {
+    width: "80%",
+    height: "5%",
+    margin: 50,
+  }
 });
+
+const THRESHOLD=25000;
+const MIN_CUTOFF=10;
+
+export default class App extends React.Component {
+  state = {
+    counter: 0,
+    lastId: 0
+  }
+  componentDidMount() {
+    RNSoundLevel.start()
+    RNSoundLevel.onNewFrame = (data) => {
+      const {rawValue, id} = data;
+      const {counter, lastId} = this.state;
+      if ((rawValue > THRESHOLD) && ((id - lastId >= MIN_CUTOFF)) || lastId === 0){
+          this.setState({counter:counter+1, lastId:id});
+      }
+    }
+    SplashScreen.hide();
+  }
+  
+  // don't forget to stop it
+  componentWillUnmount() {
+    RNSoundLevel.stop();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.number}>{this.state.counter}</Text>
+        <View style={styles.button}>
+        <Button
+          title="Reset"
+          onPress={() => this.setState({counter:0, lastId:0})}
+        />
+        </View>
+      </View>
+    );
+  }
+}
